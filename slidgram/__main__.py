c9e4db2e990651f568c01ce854701691f3660285
nicoco <nicoco@nicoco.fr>
2023-10-29 13:57:56 +0100

    chore: use new slidge entrypoint

--- slidgram/__main__.py
+++ slidgram/__main__.py
@@ -1,11 +1,8 @@
-import sys
-
-from slidge.__main__ import main as slidge_main
+from slidge import entrypoint
 
 
-def main() -> None:
-    sys.argv.extend(["--legacy", "slidgram"])
-    slidge_main()
+def main():
+    entrypoint("slidgram")
 
 
 if __name__ == "__main__":
