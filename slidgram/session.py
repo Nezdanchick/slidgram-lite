87ed13b9faa8673a6b003e728436ce9b106f9133
nicoco <nicoco@nicoco.fr>
2023-12-12 21:34:55 +0100

    feat: group creation

--- slidgram/session.py
+++ slidgram/session.py
@@ -316,5 +316,11 @@ class Session(BaseSession[int, Recipient]):
             chat_id=muc.legacy_id, message_ids=[legacy_msg_id], revoke=True
         )
 
+    async def on_create_group(self, name: str, contacts: list[Contact]) -> int:
+        chat = await self.tg.api.create_new_basic_group_chat(
+            name, [c.legacy_id for c in contacts]
+        )
+        return chat.id
+
 
 log = logging.getLogger(__name__)
